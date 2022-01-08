a12532f0c5fd1160e57361363261079c75135352
Hoang Nguyen <94545390+Hoang0904@users.noreply.github.com>
2022-01-08 15:29:01 +0700

    Add files via upload

---
+++ Checking data integrity.sql
@@ -0,0 +1,14 @@
+--Checking N/A and missing values--
+SELECT DISTINCT Id
+FROM Bellabeat_Project.dbo.dailyActivity
+SELECT DISTINCT Id
+FROM Bellabeat_Project.dbo.dailyCalories
+SELECT DISTINCT Id
+FROM Bellabeat_Project.dbo.dailyIntensities
+SELECT DISTINCT Id
+FROM Bellabeat_Project.dbo.dailySteps
+SELECT DISTINCT Id
+FROM Bellabeat_Project.dbo.sleepDay
+SELECT DISTINCT Id
+FROM Bellabeat_Project.dbo.weightLogInfo;
+
